5b6b34c16e57354a01e7ea27dd4e7ffc307ebf69
Sergey <serezha-kuznetsov@inbox.ru>
2019-07-11 15:46:32 +0500

    starter_template

---
+++ date_cars.php
@@ -0,0 +1,10 @@
+<tr>
+    <th scope="row">1</th>
+    <td>Тюриков Т.</td>
+    <td>г.Нижний Новгород</td>
+    <td>Audi R8</td>
+    <td>0</td>
+    <td>0</td>
+    <td>0</td>
+    <td>48</td>
+</tr> 
